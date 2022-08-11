93f056fda0b4dbfa072ac4419d042f18e28be641
DAIB OMOTAYO <daibomotayo@DAIBs-MBP.attlocal.net>
2022-08-11 18:44:53 -0500

    added two new ec2 instances

--- var.tf
+++ var.tf
@@ -4,6 +4,8 @@ variable "ami" {
       "production-windows" = "ami-05912b6333beaa478"
       "production-ubuntu"  = "ami-052efd3df9dad4825"
       "Development-ubuntu" = "ami-052efd3df9dad4825"
+      "Staging-ubuntu" = "ami-052efd3df9dad4825"
+      "Prestaging-ubuntu" = "ami-05912b6333beaa478"
     }
   
 }
