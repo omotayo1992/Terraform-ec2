3e49fd28e3384e2604fa937b56dc4ecc8d2017c9
DAIB OMOTAYO <daibomotayo@DAIBs-MBP.attlocal.net>
2022-08-11 18:53:25 -0500

    Destroy two ec2 instances

--- var.tf
+++ var.tf
@@ -3,8 +3,6 @@ variable "ami" {
     default = {
       "production-windows" = "ami-05912b6333beaa478"
       "production-ubuntu"  = "ami-052efd3df9dad4825"
-      "Development-ubuntu" = "ami-052efd3df9dad4825"
-      "Staging-ubuntu" = "ami-052efd3df9dad4825"
       "Prestaging-ubuntu" = "ami-05912b6333beaa478"
     }
   
