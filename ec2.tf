503c42316fcedb802112c152e4ee95fdde81f1fb
omotayo1992 <79813287+omotayo1992@users.noreply.github.com>
2022-11-02 20:17:01 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ubuntu" {
   for_each = var.ami
   ami           = each.value
   instance_type = "t2.medium"
-  key_name = "class-ec2-key"
+  key_name = "test-class"
 
 
   tags = {
